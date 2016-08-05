aa27a5d45c080f4ea086fa0f192f011194983796
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2016-08-06 00:21:21 +0200

    Memory allocator and stack smashing protector implemented

--- memmap.h
+++ memmap.h
@@ -4,9 +4,19 @@
 #include <stddef.h>
 
 void meminit(void);
+void memchk(void);
+#ifdef DEBUG
+void memstat(void);
+#else
+#define memstat() ((void)0)
+#endif
+void memfree(void);
 
-void *new(size_t n);
-void nuke(void *ptr);
-#define delete(p) do{nuke(p);p=NULL;}while(0)
+void *vnew(size_t n, const char *file, const char *func, size_t lineno, const char *desc);
+#define new2(n,desc) vnew(n,__FILE__,__func__,__LINE__,desc)
+#define new(n) vnew(n,__FILE__,__func__,__LINE__,NULL)
+void nuke(void *ptr, const char *file, const char *func, size_t lineno, const char *desc);
+#define delete2(p,desc) do{nuke(p,__FILE__,__func__,__LINE__,desc);p=NULL;}while(0)
+#define delete(p) do{nuke(p,__FILE__,__func__,__LINE__,NULL);p=NULL;}while(0)
 
 #endif
