007493e04c8735faff3574f6024ccd357770b935
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2016-09-08 12:17:18 +0200

    tools/drs: shp type, drs mapping, list inspector, item mapping
    drs now correctly shows the number of lists that are stored in a data
    file. The drs_map structure needs to be extended because the number of
    items in a list is not read correctly.
    
    Some minimal error checking is provided for out of bounds access. The
    checks are not fail-proof yet!

--- tools/drs.c
+++ tools/drs.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #define DT_WAVE   0x77617620
+#define DT_SHP    0x73687020
 #define DT_SLP    0x736c7020
 #define DT_BINARY 0x62696e61
 
@@ -19,17 +20,34 @@ struct drs {
 	uint32_t v4;
 };
 
-void drs_dump(const struct drs *this)
+struct drs_list {
+	uint32_t size;
+	uint32_t type;
+	uint32_t offset;
+};
+
+struct drs_item {
+	uint32_t id;
+	uint32_t offset;
+	uint32_t size;
+};
+
+struct drsmap {
+	char copyright[40];
+	char version[12];
+	uint32_t num34;
+	uint32_t count38;
+	//struct drs_res res[]
+};
+
+static inline void dump_type(unsigned type)
 {
-	printf(
-		"v0 = %x\n"
-		"v1 = %x\n"
-		"v2 = %x\n",
-		this->v0, this->v1, this->v2
-	);
-	switch (this->type) {
+	switch (type) {
 		case DT_WAVE:
 			puts("type = wave");
+			break;
+		case DT_SHP:
+			puts("type = shp");
 			break;
 		case DT_SLP:
 			puts("type = slp");
@@ -38,9 +56,20 @@ void drs_dump(const struct drs *this)
 			puts("type = binary");
 			break;
 		default:
-			printf("type = %x\n", this->type);
+			printf("type = %x\n", type);
 			break;
 	}
+}
+
+void drs_dump(const struct drs *this)
+{
+	printf(
+		"v0 = %x\n"
+		"v1 = %x\n"
+		"v2 = %x\n",
+		this->v0, this->v1, this->v2
+	);
+	dump_type(this->type);
 	printf(
 		"v4 = %x\n",
 		this->v4
@@ -63,7 +92,6 @@ void drs_stat(char *data, size_t size)
 void drs_find_wave(char *data, size_t size)
 {
 	size_t off;
-	char *start;
 	uint32_t magic = WAVE_CHUNK_ID;
 	FILE *f = NULL;
 	uint32_t f_size;
@@ -85,6 +113,45 @@ void drs_find_wave(char *data, size_t size)
 	if (f) fclose(f);
 }
 
+void drsmap_stat(char *data, size_t size)
+{
+	if (size < sizeof(struct drsmap)) {
+		fputs("invalid drs file\n", stderr);
+		return;
+	}
+	struct drsmap *drs = (struct drsmap*)data;
+	if (strncmp("1.00tribe", drs->version, strlen("1.00tribe"))) {
+		fputs("invalid drs file\n", stderr);
+		return;
+	}
+	printf("num34: %u\n", drs->num34);
+	printf("count38: %u\n", drs->count38);
+	if (!drs->count38)
+		return;
+	struct drs_list *list = (struct drs_list*)((char*)drs + sizeof(struct drsmap));
+	for (unsigned i = 0; i < drs->count38; ++i, ++list) {
+		printf("size: %x\n", list->size);
+		dump_type(list->type);
+		printf("offset: %x\n", list->offset);
+		// resource offset or something
+		if (list->offset > size) {
+			fputs("bad item offset\n", stderr);
+			return;
+		}
+		struct drs_item *item = (struct drs_item*)(data + list->offset);
+		for (unsigned j = 0; j < list->size; ++j, ++item) {
+			if (list->offset + (j + 1) * sizeof(struct drs_item) > size) {
+				fputs("bad list\n", stderr);
+				return;
+			}
+			printf("#%u\n", j);
+			printf("item ID: %u\n", item->id);
+			printf("offset: %u\n", item->offset);
+			printf("size: %u\n", item->size);
+		}
+	}
+}
+
 static int process(char *name)
 {
 	int ret = 1, fd = -1;
@@ -101,8 +168,7 @@ static int process(char *name)
 		perror("mmap");
 		goto fail;
 	}
-	drs_stat(map, mapsz);
-	drs_find_wave(map, mapsz);
+	drsmap_stat(map, mapsz);
 	ret = 0;
 fail:
 	if (map != MAP_FAILED)
