55af78fa95e89dc67c9fd40ed8ffc3a0b267741b
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2016-12-08 16:47:24 +0100

    src/lang: updated string table

---
+++ src/lang.h
@@ -0,0 +1,137 @@
+#ifndef AOE_LANG_H
+#define AOE_LANG_H
+
+#define STR_FIRST                   101
+#define STR_FONT0_0                 102
+#define STR_FONT1                   103
+#define STR_FONT2_0                 104
+#define STR_FONT0_1                 110
+#define STR_FONT_SIZE0              111
+#define STR_SHORTCUT_B0             112
+#define STR_FONT0_2                 113
+#define STR_FONT_SIZE1              114
+#define STR_SHORTCUT_B1             115
+#define STR_FONT0_3                 116
+#define STR_FONT_SIZE2              117
+#define STR_SHORTCUT_B2             118
+#define STR_FONT2_1                 119
+#define STR_FONT_SIZE3              120
+#define STR_SHORTCUT_N0             121
+#define STR_FONT2_2                 122
+#define STR_FONT_SIZE4              123
+#define STR_SHORTCUT_B3             124
+#define STR_FONT2_3                 125
+#define STR_FONT_SIZE5              126
+#define STR_SHORTCUT_N1             127
+#define STR_FONT2_4                 128
+#define STR_FONT_SIZE6              129
+#define STR_SHORTCUT_B4             130
+#define STR_FONT2_5                 131
+#define STR_FONT_SIZE7              132
+#define STR_SHORTCUT_N2             133
+
+#define STR_TITLE                  1001
+#define STR_CROSS                  1002
+#define STR_HELP                   1003
+#define STR_LOAD_GAME              1101
+#define STR_INIT_SCENARIO          1102
+#define STR_LOAD_SCENARIO          1103
+#define STR_WAIT_PLAYERS           1108
+#define STR_READY                  1200
+#define STR_COMPUTER               1201
+#define STR_EMPTY                  1202
+#define STR_LOAD                   1203
+#define STR_WAIT                   1204
+#define STR_UNKNOWN                1205
+#define STR_SAVE_SCENARIO          1206
+#define STR_COMM_PLAYER_IDLE       1210
+
+#define STR_ERROR                  2001
+#define STR_ERR_INIT               2002
+#define STR_ERR_GFX                2003
+#define STR_ERR_SFX                2004
+#define STR_ERR_DIRECTX            2005
+#define STR_ERR_COMM               2005
+#define STR_ERR_OPENGL             STR_ERR_DIRECTX
+#define STR_ERR_CREATE_GAME        2006
+#define STR_ERR_LOAD_DATA          2007
+#define STR_ERR_NO_CD              2008
+#define STR_ERR_MULTI_CD           2009
+#define STR_ERR_NO_CD_SCENARIO     2010
+#define STR_ERR_PAL                2011
+#define STR_ERR_SYNC               2012
+#define STR_ERR_DX                 2013
+#define STR_ERR_LOWOS              2014
+#define STR_ERR_LOWDISK            2015
+#define STR_ERR_NO_ORIG            2016
+#define STR_ERR_EXPIRED            2017
+
+#define STR_MSG_CPU                2300
+#define STR_CPU_VENGEANCE          2301
+#define STR_CPU_GOOD               2302
+#define STR_CPU_TOGETHER           2303
+#define STR_CPU_ALONE              2304
+#define STR_CPU_YOU_MUST_DIE       2305
+#define STR_CPU_MORE_GOLD_OR_WAR   2306
+#define STR_CPU_GOLD_OR_WAR        2307
+#define STR_CPU_GOLD_AND_ALLY      2308
+#define STR_CPU_MORE_GOLD_AND_ALLY 2309
+#define STR_CPU_THANKS_ANYWAY      2310
+#define STR_CPU_ALLEGIANCE         2311
+#define STR_CPU_HELP               2312
+#define STR_CPU_JOIN_ME            2313
+
+#define STR_TRIBE                  2400
+#define STR_FAIL_START             2401
+#define STR_FAIL_SAVE_GAME         2402
+#define STR_FAIL_SAVE_SCENARIO     2403
+#define STR_FAIL_LOAD_GAME         2404
+#define STR_FAIL_LOAD_SCENARIO     2405
+#define STR_FAIL_SYNC              2406
+#define STR_LOW_FOOD               3001
+#define STR_LOW_WOOD               3002
+#define STR_LOW_STONE              3003
+#define STR_LOW_GOLD               3004
+#define STR_LOW_POP                3005
+#define STR_SHIP_TRADE             3006
+#define STR_SHIP_LAND              3007
+#define STR_SHIP_UNLOAD            3008
+
+#define STR_TRIBUTE_GOLD           3009
+
+#define STR_FAIL_PLACE             3010
+#define STR_FAIL_TRADE             3011
+#define STR_PLAYER_DISCONNECT      3012
+#define STR_PLAYER_RESIGNED        3013
+#define STR_PLAYER_DEFEATED        3014
+
+#define STR_TRIBUTE_WOOD           3015
+#define STR_TRIBUTE_FOOD           3016
+#define STR_TRIBUTE_STONE          3017
+
+#define STR_FAIL_REPAIR            3018
+#define STR_LOW_POP_MAX            3046
+
+#define STR_AGE_STONE              4201
+#define STR_AGE_TOOL               4202
+#define STR_AGE_BRONZE             4203
+#define STR_AGE_IRON               4204
+#define STR_AGE_IRON2              4205
+#define STR_AGE_NOMAD              4206
+#define STR_FOOD                   4301
+#define STR_WOOD                   4302
+#define STR_STONE                  4303
+#define STR_GOLD                   4304
+
+#define STR_MAP_GENERATE          10660
+#define STR_MAP_GENERATE_CAPS     10661
+
+#define STR_MAP_BRUSH_SIZE        10671
+#define STR_MAP_BRUSH_TINY        10672
+#define STR_MAP_BRUSH_SMALL       10673
+#define STR_MAP_BRUSH_MEDIUM      10674
+#define STR_MAP_BRUSH_LARGE       10675
+#define STR_MAP_BRUSH_HUGE        10676
+#define STR_MAP_TERRAIN           10684
+
+#endif
