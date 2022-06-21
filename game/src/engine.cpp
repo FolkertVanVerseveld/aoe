c62109661bd5141cdff4c9b00fbbb3e389e48945
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2022-06-22 01:07:01 +0200

    change title, make engine unique, add menubar

--- game/src/engine.cpp
+++ game/src/engine.cpp
@@ -4,7 +4,6 @@
 #include <imgui_impl_sdl.h>
 #include <imgui_impl_opengl3.h>
 
-#include <stdio.h>
 #include <SDL2/SDL.h>
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <SDL2/SDL_opengles2.h>
@@ -12,10 +11,55 @@
 #include <SDL2/SDL_opengl.h>
 #endif
 
+#include <cstdio>
+
+#include <mutex>
+#include <memory>
+
 namespace aoe {
 
-Engine::Engine() {}
-Engine::~Engine() {}
+Engine *eng;
+std::mutex m_eng;
+
+Engine::Engine() : show_demo(false) {
+	std::lock_guard<std::mutex> lk(m_eng);
+	if (eng)
+		throw std::runtime_error("there can be only one");
+
+	eng = this;
+}
+
+Engine::~Engine() {
+	std::lock_guard<std::mutex> lk(m_eng);
+	assert(eng);
+	eng = nullptr;
+}
+
+void Engine::show_menubar() {
+	if (!ImGui::BeginMainMenuBar())
+		return;
+
+	if (ImGui::BeginMenu("File")) {
+		if (ImGui::MenuItem("Quit"))
+			exit(0);
+
+		ImGui::EndMenu();
+	}
+
+	if (ImGui::BeginMenu("View")) {
+		ImGui::Checkbox("Demo window", &show_demo);
+		ImGui::EndMenu();
+	}
+
+	ImGui::EndMainMenuBar();
+}
+
+void Engine::display() {
+	show_menubar();
+
+	if (show_demo)
+		ImGui::ShowDemoWindow(&show_demo);
+}
 
 int Engine::mainloop() {
 	// Setup SDL
@@ -56,7 +100,7 @@ int Engine::mainloop() {
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-	SDL_Window *window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+	SDL_Window *window = SDL_CreateWindow("Age of Empires", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, window_flags);
 	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 	SDL_GL_MakeCurrent(window, gl_context);
 	SDL_GL_SetSwapInterval(1); // Enable vsync
@@ -69,8 +113,9 @@ int Engine::mainloop() {
 	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
 	// Setup Dear ImGui style
-	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsDark();
 	//ImGui::StyleColorsClassic();
+	ImGui::StyleColorsLight();
 
 	// Setup Platform/Renderer backends
 	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
@@ -92,8 +137,6 @@ int Engine::mainloop() {
 	//IM_ASSERT(font != NULL);
 
 	// Our state
-	bool show_demo_window = true;
-	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	// Main loop
@@ -120,10 +163,6 @@ int Engine::mainloop() {
 		ImGui_ImplSDL2_NewFrame();
 		ImGui::NewFrame();
 
-		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-		if (show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window);
-
 		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 		{
 			static float f = 0.0f;
@@ -132,8 +171,6 @@ int Engine::mainloop() {
 			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
 			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
 
 			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 			ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
@@ -147,15 +184,7 @@ int Engine::mainloop() {
 			ImGui::End();
 		}
 
-		// 3. Show another simple window.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
+		display();
 
 		// Rendering
 		ImGui::Render();
--- game/src/engine.hpp
+++ game/src/engine.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
 #include <mutex>
-#include <memory>
 
 namespace aoe {
 
 class Engine final {
+	bool show_demo;
 public:
 	Engine();
 	~Engine();
 
 	int mainloop();
+private:
+	void display();
+	void show_menubar();
 };
+
+extern Engine *eng;
+extern std::mutex m_eng;
 
 }
