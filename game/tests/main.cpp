33994170bccdd3def62fb4070d99c80f01809d72
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2022-06-22 01:24:09 +0200

    update aoe engine test

--- game/tests/main.cpp
+++ game/tests/main.cpp
@@ -1,4 +1,29 @@
+#include "../src/engine.hpp"
+
+namespace aoe {
+
+static void engine_create_delete() {
+	Engine eng;
+	(void)eng;
+}
+
+static void engine_create_twice() {
+	Engine eng1;
+	try {
+		Engine eng2;
+		fprintf(stderr, "%s: created twice\n", __func__);
+	} catch (std::runtime_error &e) {}
+}
+
+static void runall() {
+	engine_create_delete();
+	engine_create_twice();
+}
+
+}
+
 int main(void)
 {
+	aoe::runall();
 	return 0;
 }
