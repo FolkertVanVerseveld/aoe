44ceac92b40a2ffee6d8c6924c745802a6c0180c
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2023-04-05 23:42:54 +0200

    decompress scenario data

--- game/src/legacy/scenario.cpp
+++ game/src/legacy/scenario.cpp
@@ -1,9 +1,13 @@
 #include "scenario.hpp"
 
+#include <cstddef>
 #include <cassert>
 #include <fstream>
 
 #include "../engine/endian.h"
+
+// yes, including .c's is evil, but... we have no choice :/
+#include <miniz.c>
 
 namespace aoe {
 
@@ -50,7 +54,47 @@ void Scenario::load(const char *path) {
 
 	players = le32toh(players);
 
-	// TODO read compressed data
+	// read compressed data
+
+	// 8 + length is where compressed scenario data starts
+	unsigned long long data_start = 8 + length;
+
+	in.seekg(data_start);
+
+	// make a very rough lazy estimate how much the uncompressed data will be
+	// yeah we could try to increase it step-wise when it fails, but it's legacy anyways so unless it breaks often, i won't bother
+	size_t max_inflated_size = 64 * 1024 * 1024;
+
+	std::vector<uint8_t> inflated(max_inflated_size);
+
+	unsigned long inflated_size = inflated.size();
+
+	mz_stream stream{ 0 };
+
+	stream.next_in = data.data() + data_start;
+	stream.avail_in = data.size() - data_start;
+
+	stream.next_out = inflated.data();
+	stream.avail_out = inflated.size();
+
+	int status;
+
+	if ((status = mz_inflateInit2(&stream, -MZ_DEFAULT_WINDOW_BITS))) {
+		fprintf(stderr, "mz v%s failed: %s\n", mz_version(), mz_error(status));
+		return;
+	}
+
+	status = mz_inflate(&stream, MZ_FINISH);
+	mz_inflateEnd(&stream);
+
+	if (status != MZ_STREAM_END) {
+		fprintf(stderr, "inflate error: %s\n", mz_error(status));
+		return;
+	}
+
+	inflated.resize(stream.total_out);
+
+	data = std::move(inflated);
 }
 
 }
