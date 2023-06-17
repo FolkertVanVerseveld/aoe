68c028830876c819004e3bf3514c75d951e6c2ab
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2023-06-17 15:57:13 +0200

    extract entity from game.hpp

--- game/src/game.hpp
+++ game/src/game.hpp
@@ -11,8 +11,6 @@
 #include <mutex>
 #include <set>
 
-#include "../engine/audio.hpp"
-
 #include "world/terrain.hpp"
 
 #include <idpool.hpp>
@@ -25,74 +23,6 @@
 #include "world/entity.hpp"
 
 namespace aoe {
-
-class WorldView;
-
-class Entity final {
-public:
-	IdPoolRef ref;
-	EntityType type;
-
-	unsigned playerid;
-	float x, y, angle;
-
-	IdPoolRef target_ref; // if == invalid_ref, use target_x,target_y
-	float target_x, target_y;
-
-	float subimage;
-	EntityState state;
-	bool xflip;
-
-	EntityStats stats;
-
-	Entity(IdPoolRef ref);
-	Entity(IdPoolRef ref, EntityType type, unsigned playerid, float x, float y, float angle=0.0f, EntityState state=EntityState::alive);
-	// resource only
-	Entity(IdPoolRef ref, EntityType type, float x, float y, unsigned subimage);
-
-	Entity(const EntityView &ev);
-
-	friend bool operator<(const Entity &lhs, const Entity &rhs) noexcept {
-		return lhs.ref < rhs.ref;
-	}
-
-	bool die() noexcept;
-	void decay() noexcept;
-
-	bool tick(WorldView&) noexcept;
-	bool hit(WorldView&, Entity &aggressor) noexcept;
-
-	unsigned get_atk(const EntityStats &stats) noexcept;
-
-	constexpr bool is_alive() const noexcept {
-		return state != EntityState::dying && state != EntityState::decaying;
-	}
-
-	bool task_cancel() noexcept;
-
-	bool task_move(float x, float y) noexcept;
-	bool task_attack(Entity&) noexcept;
-	/** ensure specified type can be trained at this entity. */
-	bool task_train_unit(EntityType) noexcept;
-
-	std::optional<SfxId> sfxtick() noexcept;
-	const EntityBldInfo &bld_info() const;
-	const EntityImgInfo &img_info() const;
-
-	bool imgtick(unsigned n) noexcept;
-private:
-	void reset_anim() noexcept;
-	bool set_state(EntityState) noexcept;
-	bool try_state(EntityState) noexcept;
-	bool move() noexcept;
-	bool attack(WorldView&) noexcept;
-	bool in_range(const Entity&) const noexcept;
-
-	/* Compute facing angle and return euclidean distance. */
-	float lookat(float x, float y) noexcept;
-
-	void set_type(EntityType type, bool resethp=false);
-};
 
 class PlayerAchievements final {
 public:
--- game/src/world/entity.hpp
+++ game/src/world/entity.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <idpool.hpp>
+#include <optional>
+
+#include "../engine/audio.hpp"
 
 #include "entity_info.hpp"
 
@@ -55,6 +58,74 @@ public:
 	EntityTask(EntityTaskType type, IdPoolRef ref1, IdPoolRef ref2);
 	// train unit
 	EntityTask(IdPoolRef ref, EntityType t);
+};
+
+class WorldView;
+
+class Entity final {
+public:
+	IdPoolRef ref;
+	EntityType type;
+
+	unsigned playerid;
+	float x, y, angle;
+
+	IdPoolRef target_ref; // if == invalid_ref, use target_x,target_y
+	float target_x, target_y;
+
+	float subimage;
+	EntityState state;
+	bool xflip;
+
+	EntityStats stats;
+
+	Entity(IdPoolRef ref);
+	Entity(IdPoolRef ref, EntityType type, unsigned playerid, float x, float y, float angle=0.0f, EntityState state=EntityState::alive);
+	// resource only
+	Entity(IdPoolRef ref, EntityType type, float x, float y, unsigned subimage);
+
+	Entity(const EntityView &ev);
+
+	friend bool operator<(const Entity &lhs, const Entity &rhs) noexcept {
+		return lhs.ref < rhs.ref;
+	}
+
+	bool die() noexcept;
+	void decay() noexcept;
+
+	bool tick(WorldView&) noexcept;
+	bool hit(WorldView&, Entity &aggressor) noexcept;
+
+	unsigned get_atk(const EntityStats &stats) noexcept;
+
+	constexpr bool is_alive() const noexcept {
+		return state != EntityState::dying && state != EntityState::decaying;
+	}
+
+	bool task_cancel() noexcept;
+
+	bool task_move(float x, float y) noexcept;
+	bool task_attack(Entity&) noexcept;
+	/** ensure specified type can be trained at this entity. */
+	bool task_train_unit(EntityType) noexcept;
+
+	std::optional<SfxId> sfxtick() noexcept;
+	const EntityBldInfo &bld_info() const;
+	const EntityImgInfo &img_info() const;
+
+	bool imgtick(unsigned n) noexcept;
+private:
+	void reset_anim() noexcept;
+	bool set_state(EntityState) noexcept;
+	bool try_state(EntityState) noexcept;
+	bool move() noexcept;
+	bool attack(WorldView&) noexcept;
+	bool in_range(const Entity&) const noexcept;
+
+	/* Compute facing angle and return euclidean distance. */
+	float lookat(float x, float y) noexcept;
+
+	void set_type(EntityType type, bool resethp=false);
 };
 
 
