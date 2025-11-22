6d8f4f36a41005fe720bc92817c05a70baeb29d4
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2025-11-22 15:18:11 +0000

    nicer fatal error handling

--- game/main.cpp
+++ game/main.cpp
@@ -5,18 +5,38 @@
 #include <memory>
 #include <stdexcept>
 
+#if _WIN32
+#include <Windows.h>
+
+static void show_error(const char *title, const char *message)
+{
+	fprintf(stderr, "%s: %s\n", title, message);
+	MessageBox(NULL, message, title, MB_ICONERROR | MB_OK);
+}
+#else
+#include <SDL2/SDL_messagebox.h>
+
+static void show_error(const char *title, const char *message)
+{
+	fprintf(stderr, "%s: %s\n", title, message);
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, message, NULL);
+}
+#endif
+
 int main(int argc, char **argv)
 {
 	try {
 		std::unique_ptr<aoe::Engine> eng(new aoe::Engine());
 		return eng->mainloop();
-#if 0
+#if (defined(_DEBUG) && !_DEBUG) || (defined(NDEBUG) && !NDEBUG)
 	} catch (const std::runtime_error &e) {
-		fprintf(stderr, "runtime error: %s\n", e.what());
+		show_error("Fatal Runtime Error", e.what());
 	} catch (const std::exception &e) {
-		fprintf(stderr, "internal error: %s\n", e.what());
+		show_error("Fatal Internal Error", e.what());
 #endif
 	} catch (int v) {
+		// a la python: interpret as exit code
+		// so we can do something 'clean' like: throw 0;
 		return v;
 	}
 	return 1;
